b59bbf73feafadbf58faff89f3bf2ffb7e926148
Paulina <paulina_prokop@wp.pl>
2016-07-03 12:21:39 +0200

    Url changes removed - incompatibility with browsers

--- js/main.js
+++ js/main.js
@@ -30,16 +30,16 @@ $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-$(document).bind('scroll',function(e){
-    $('section').each(function(){
-        if (
-            $(this).offset().top < window.pageYOffset + 10
-//begins before top
-            && $(this).offset().top + $(this).height() > window.pageYOffset + 10
-//but ends in visible area
-//+ 10 allows you to change hash before it hits the top border
-        ) {
-            window.location.hash = $(this).attr('id');
-        }
-    });
-});
+// $(document).bind('scroll',function(e){
+//     $('section').each(function(){
+//         if (
+//             $(this).offset().top < window.pageYOffset + 20
+// //begins before top
+//             && $(this).offset().top + $(this).height() > window.pageYOffset + 20
+// //but ends in visible area
+// //+ 10 allows you to change hash before it hits the top border
+//         ) {
+//             window.location.hash = $(this).attr('id');
+//         }
+//     });
+// });
