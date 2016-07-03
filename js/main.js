0cd7f38e9a283720ae9808a754055035d17cc05b
Paulina <paulina_prokop@wp.pl>
2016-07-03 14:48:44 +0200

    Enhancements

--- js/main.js
+++ js/main.js
@@ -14,18 +14,16 @@ $(document).ready(function(){
     var offset = startchange.offset();
     $(document).scroll(function() {
         scroll_start = $(this).scrollTop();
-        if(scroll_start > offset.top) {
-            $('.navbar-fixed-top').css('background', '#FFFFFF');
-        } else {
-            $('.navbar-fixed-top').css('background', 'transparent');
+        if ( $(window).width() > 768) {
+            if (scroll_start > offset.top) {
+                $('.navbar-fixed-top').css('background', '#FFFFFF');
+            } else {
+                $('.navbar-fixed-top').css('background', 'transparent');
+            }
         }
     });
 });
 
 $('body').scrollspy({
     target: '.navbar-fixed-top'
-});
-
-$('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
-    $('.navbar-toggle:visible').click();
 });
